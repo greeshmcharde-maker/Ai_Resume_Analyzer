a97823b31efb3fd882506bbbbb795897ec2cadfe
Greeshm Charde <greeshmcharde@gmail.com>
2026-08-03 22:49:55 +0530

    Refactor glass_card function to use dedent

--- ui/glass.py
+++ ui/glass.py
@@ -1,25 +1,28 @@
 import streamlit as st
+from textwrap import dedent
 
 def glass_card(icon, title, value, subtitle=""):
 
-    st.markdown(f"""
-    <div class="glass-card">
+    html = dedent(f"""
+<div class="glass-card">
 
-        <div class="glass-icon">
-            {icon}
-        </div>
-
-        <div class="glass-title">
-            {title}
-        </div>
-
-        <div class="glass-value">
-            {value}
-        </div>
-
-        <div class="glass-subtitle">
-            {subtitle}
-        </div>
-
+    <div class="glass-icon">
+        {icon}
     </div>
-    """, unsafe_allow_html=True)
+
+    <div class="glass-title">
+        {title}
+    </div>
+
+    <div class="glass-value">
+        {value}
+    </div>
+
+    <div class="glass-subtitle">
+        {subtitle}
+    </div>
+
+</div>
+""")
+
+    st.markdown(html, unsafe_allow_html=True)
